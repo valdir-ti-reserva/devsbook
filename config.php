fb0a9a88fbff2df84e152c18212877c486aa5667
valdir-ti <valdir.ti@gmail.com>
2020-09-08 16:39:01 -0300

    Configurando o tempo de Sessão do usuário

--- config.php
+++ config.php
@@ -1,6 +1,25 @@
 <?php
 
 session_start();
+
+$timeout = 600; // Tempo da sessao em segundos
+ 
+// Verifica se existe o parametro timeout
+if(isset($_SESSION['timeout'])) {
+    // Calcula o tempo que ja se passou desde a cricao da sessao
+    $duracao = time() - (int) $_SESSION['timeout'];
+  
+  	// Verifica se ja expirou o tempo da sessao
+    if($duracao > $timeout) {
+        // Destroi a sessao e cria uma nova
+        session_destroy();
+        session_start();
+    }
+}
+ 
+// Atualiza o timeout.
+$_SESSION['timeout'] = time();
+
 date_default_timezone_set('America/Sao_Paulo');
 
 $base = 'http://localhost:81/devsbook';
